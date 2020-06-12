a8bb4a67c42fb7a99009b7aa706ccc388d53aca9
kdnakt <a.kid.1985@gmail.com>
2020-06-12 14:16:35 +0000

    add fork.c

---
+++ chap03/fork.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+static void child() {
+    printf("I'm child! My pid is %d.\n", getpid());
+    exit(EXIT_SUCCESS);
+}
+
+static void parent(pid_t pid_c) {
+    printf("I'm parent! My pid is %d and the pid of my child is %d.\n", getpid(), pid_c);
+    exit(EXIT_SUCCESS);
+}
+
+int main(void) {
+    pid_t ret;
+    ret = fork();
+    if (ret == -1)
+        err(EXIT_FAILURE, "fork() failed");
+    if (ret == 0) {
+        // child process came here because fork() returns 0 for child process
+        child();
+    } else {
+        // parent process came here because fork() returns the newly created child process > 1
+        parent(ret);
+    }
+    // shouldn't reach here
+    err(EXIT_FAILURE, "shouldn't reach here");
+}
