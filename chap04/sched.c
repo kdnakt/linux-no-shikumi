37ddb03a0e67e5d42eee2a5ca5eef64441cd9d12
kdnakt <a.kid.1985@gmail.com>
2020-06-15 04:41:22 +0000

    fix include order

--- chap04/sched.c
+++ chap04/sched.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <err.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <err.h>
 
 #define NLOOP_FOR_ESTIMATION 1000000000UL
 #define NSECS_PER_MSEC 1000000UL
